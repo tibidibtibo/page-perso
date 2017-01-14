b01dfd46ab48c7332acb91facaa148a693d39eca
Thibault de Boissieu <thibault@de-boissieu.fr>
2017-01-14 19:28:21 +0100

    Design

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -41,7 +41,7 @@ module.exports = {
       {
         test: /\.scss$/,
         exclude: /node_modules/,
-        loaders: ['raw-loader', 'sass-loader'] // sass-loader not scss-loader
+        loaders: ['raw-loader', 'sass-loader'], // sass-loader not scss-loader
       },
       {  
         test: /bootstrap\/dist\/js\/umd\//, 
